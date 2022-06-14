5d5cc5b9c13e745118e441bb0c9825d4062aafaa
anhddse161412 <anhddse161412@fpt.edu.vn>
2022-06-14 21:58:18 +0700

    Components Part 3_2

--- src/components/Hello2.js
+++
@@ -1,8 +0,0 @@
-export default function Hello2(props) {
-   return (
-      <div>
-         <h1>Pass mon 10d, {props.msg}</h1>
-         <h1>{props.user.id}</h1>
-      </div>
-   );
-}
---
+++ src/components/MenuComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import DishDetail from "./DishdetailComponent";
+class Menu extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         selectedDish: null,
+      }; //end state
+   } //end constructor
+   onDishSelect(dish) {
+      this.setState({ selectedDish: dish });
+   } //end onDishSelect
+   // renderDish(dish) {
+   //    console.log(dish);
+   //    if (dish != null)
+   //       return (
+   //          <Card>
+   //             <CardImg src={dish.image} alt={dish.name} />
+   //             <CardBody>
+   //                <CardTitle>{dish.name}</CardTitle>
+   //                <CardText>{dish.description}</CardText>
+   //             </CardBody>
+   //          </Card>
+   //       );
+   //    else return <div></div>;
+   // } //end renderDish
+   render() {
+      const menu = this.props.dishes.map((dish) => {
+         return (
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+               <Card onClick={() => this.onDishSelect(dish)}>
+                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                  <CardImgOverlay>
+                     <CardTitle>{dish.name}</CardTitle>
+                  </CardImgOverlay>
+               </Card>
+            </div>
+         );
+      });
+
+      return (
+         <div className="container">
+            <div className="row">{menu}</div>
+            <div className="row">
+               <DishDetail dish={this.state.selectedDish} />
+            </div>
+         </div>
+      );
+   }
+}
+export default Menu;

a01ac64c1c9b3efb1f56e756e777b9b8b3f12742
anhddse161412 <anhddse161412@fpt.edu.vn>
2022-06-14 19:59:29 +0700

    Components Part 3_1

--- src/App.js
+++ src/App.js
@@ -1,62 +1,18 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import Hello from "./components/Hello";
-import Hello2 from "./components/Hello2";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 function App() {
-   const data = {
-      id: "1",
-      name: "name",
-   };
    return (
-      //     <div className="App">
-      //       <header className="App-header">
-      //         <img src={logo} className="App-logo" alt="logo" />
-      //         <p>
-      //           Edit <code>src/App.js</code> and save to reload.
-      //         </p>
-      //         <a
-      //           className="App-link"
-      //           href="https://reactjs.org"
-      //           target="_blank"
-      //           rel="noopener noreferrer"
-      //         >
-      //           Learn React
-      //         </a>
-      //       </header>
-      //     </div>
       <div className="App">
          <Navbar dark color="primary">
             <div className="container">
                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
          </Navbar>
+         <Menu />
       </div>
    );
 }
-
-// function formatName(user) {
-//    return user.firstName + " " + user.lastName;
-// }
-
-// const user = {
-//    firstName: "Harper",
-//    lastName: "Perez",
-// };
-
-// const element = <h1> Hello, {formatName(user)}</h1>;
-
-// function App() {
-//    const name = "Josh";
-//    //  const element = <h1>hello, {name}</h1>;
-
-//    return (
-//       <div className="App">
-//          <h1>Hello world</h1>
-//          {element}
-//       </div>
-
-//    );
-// }
 
 export default App;
